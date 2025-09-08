ce5ab878657abe5c7d0f434479ad287082281f5c
Sat Garcia <sat@sandiego.edu>
2025-09-08 15:34:10 -0700

    add (incomplete) file for drawing a decagon.

---
+++ draw_decagon.py
@@ -0,0 +1,17 @@
+"""
+Module: draw_decagon
+
+Program to draw a regular decagon (i.e. 10-sided polygon) using turtles.
+"""
+
+import turtle
+
+# create a turtle and set the pen color
+sat = turtle.Turtle()
+sat.pencolor("red")
+
+# to do: draw the decagon
+
+# keep the turtle window open until we click on it
+turtle_window = turtle.Screen()
+turtle_window.exitonclick()
